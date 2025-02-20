e1fa4c4193073d4044ea227062391d0225b02a50
Zodiacal0 <javierherrera5513@gmail.com>
2025-02-19 18:15:37 -0600

    doc($documentation): se documentó con swagger

--- configs/server.js
+++ configs/server.js
@@ -10,6 +10,7 @@ import userRoutes from "../src/user/user.routes.js"
 import publicationRoutes from "../src/publication/publication.routes.js";
 import commentRoutes from  "../src/comment/comment.routes.js";
 import { userSeeder } from "../src/seeders/user.seeder.js";
+import { swaggerDocs, swaggerUi } from "./swagger.js";
 
 const middlewares = (app) => {
     app.use(express.urlencoded({extended:false}));
@@ -25,6 +26,7 @@ const routes = (app) =>{
     app.use("/commentManager/v1/user", userRoutes);
     app.use("/commentManager/v1/publication", publicationRoutes);
     app.use("/commentManager/v1/comment", commentRoutes);
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 }
 const connectionMongo = async() =>{
     try{
